e99466533605b76a111094cf281ba6b854f0bf52
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-02-24 18:43:01 +0100

    title ok

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<!-- javascript -->
+<script>
+
+</script>
+
+
+<!-- html -->
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+
+<!-- CSS -->
+<style>
+h1 {
+    text-align: center;
+    padding: 30px;
+}
+</style>
